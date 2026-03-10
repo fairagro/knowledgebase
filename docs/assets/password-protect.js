a8d7a02504cefd06750dc23f8f2c29c10acf8926
Paul <paul.peschel@gmail.com>
2026-03-10 14:43:51 +0100

    pw removed

--- docs/assets/password-protect.js
+++ docs/assets/password-protect.js
@@ -1,14 +1,1 @@
-(function() {
-  var password = "fairagro@@";
-  var isAuthenticated = sessionStorage.getItem('authenticated') === 'true';
-  
-  if (!isAuthenticated) {
-    var userPassword = prompt("Please enter the password to access this site:");
-    if (userPassword === password) {
-      sessionStorage.setItem('authenticated', 'true');
-    } else {
-      document.body.innerHTML = '<h1 style="text-align:center;margin-top:20%;">Access Denied</h1>';
-      throw new Error('Access denied');
-    }
-  }
-})();
+
